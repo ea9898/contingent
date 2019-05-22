93f25c0f942011767872a99064f3e1950c8db3f1
ddryuchin <dd@ptnl.moscow>
2019-05-22 09:34:44 +0300

    Корректировка БД в соответсвтии с согласованной схемой

---
+++ contingent/application/contingent-area/src/test/moscow/ptnl/contingent2/area/AreaCheckerTests.java
@@ -0,0 +1,18 @@
+package moscow.ptnl.contingent2.area;
+
+import moscow.ptnl.contingent.area.entity.nsi.AreaTypes;
+import moscow.ptnl.contingent.area.error.Validation;
+import moscow.ptnl.contingent.area.repository.nsi.AreaTypesCRUDRepository;
+import moscow.ptnl.contingent.area.service.AreaChecker;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public class AreaCheckerTests {
+
+}
