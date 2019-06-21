80bc1a42773a488e556d36ebd0ef7cf0791d4e6b
ddryuchin <dd@ptnl.moscow>
2019-06-21 11:00:55 +0300

    Spring Batch

---
+++ contingent/application/contingent-area-infrastructure/src/main/java/moscow/ptnl/contingent/configuration/BatchConfig.java
@@ -0,0 +1,25 @@
+package moscow.ptnl.contingent.configuration;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+    @Autowired
+    private JobBuilderFactory jobs;
+
+    @Autowired
+    private StepBuilderFactory steps;
+
+    @Scheduled(fixedRate = 60000)
+    public void schedule() {
+        System.out.println("Task test... every 60 seconds");
+    }
+
+}
