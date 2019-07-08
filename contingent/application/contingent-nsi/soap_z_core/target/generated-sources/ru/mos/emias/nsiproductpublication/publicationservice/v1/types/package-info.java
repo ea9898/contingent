5071fa9be0c4fce453922cec00e136fa998155b5
ddryuchin <dd@ptnl.moscow>
2019-07-08 10:22:21 +0300

    CONTINGENT2-4 Внедрение механизма обновления НСИ в рамках работ по Контингент-2

--- contingent/application/contingent-nsi/soap_z_core/target/generated-sources/ru/mos/emias/nsiproductpublication/publicationservice/v1/types/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://emias.mos.ru/nsiProductPublication/publicationService/v1/types/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package ru.mos.emias.nsiproductpublication.publicationservice.v1.types;
--- contingent/application/contingent-nsi/soap_z_core/target/generated-sources/ru/mos/op/receive_changes/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://op.mos.ru/receive_changes", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package ru.mos.op.receive_changes;
