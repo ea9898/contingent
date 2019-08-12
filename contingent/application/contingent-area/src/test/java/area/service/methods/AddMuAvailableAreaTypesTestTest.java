7c210701772255ab469ebff13c5c9aec2cd46c30
ddryuchin <dd@ptnl.moscow>
2019-08-12 15:45:41 +0300

    CONTINGENT2-259 Тестирование методов К_УУ_4, К_УУ_5, К_УУ_6

---
+++ contingent/application/contingent-area/src/test/java/area/service/methods/AddMuAvailableAreaTypesTestTest.java
@@ -0,0 +1,143 @@
+package area.service.methods;
+
+import moscow.ptnl.contingent.area.entity.area.MoAvailableAreaTypes;
+import moscow.ptnl.contingent.area.entity.area.MuAvailableAreaTypes;
+import moscow.ptnl.contingent.area.entity.nsi.AreaType;
+import moscow.ptnl.contingent.area.error.ContingentException;
+import moscow.ptnl.contingent.area.error.Validation;
+import moscow.ptnl.contingent.area.error.ValidationMessage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * (К_УУ_4)	Добавление типов, доступных для МУ
+ */
+public class AddMuAvailableAreaTypesTestTest extends MuAvailableAreaTypesTest {
+    /**
+     * С_УУ_73
+     */
+    @Test
+    public void test_C_YY_73_1() {
+        Mockito.doReturn(new ArrayList<>()).when(moAvailableAreaTypesRepository).findAreaTypes(2L);
+        Mockito.doReturn(new ArrayList<>()).when(muAvailableAreaTypesRepository).findAreaTypes(3L);
+
+        Mockito.doReturn(Optional.of(areaType10)).when(areaTypesCRUDRepository).findById(10L);
+        Mockito.doReturn(Optional.of(areaType20)).when(areaTypesCRUDRepository).findById(20L);
+
+        try {
+            areaServiceInternal.addMuAvailableAreaTypes(2L, 3L, Arrays.asList(10L, 20L));
+        } catch (ContingentException e) {
+            Validation validation = e.getValidation();
+            List<ValidationMessage> validationMessageList = e.getValidation().getMessages();
+            if (validationMessageList.size() != 2) {
+                Assertions.fail("Должно отсутствовать 2 типа участков.");
+            }
+            if (!validationMessageList.stream().allMatch(vm -> vm.getCode().equals("E026"))) {
+                Assertions.fail("Должна быть ошибка только E026.");
+            }
+            if (validationMessageList.stream().map(ValidationMessage::getParameters).noneMatch(vp -> vp.get(0).getValue().equals("Тестовый тип участка 10"))) {
+                Assertions.fail("Должна быть ошибка с типом участка 10.");
+            }
+            if (validationMessageList.stream().map(ValidationMessage::getParameters).noneMatch(vp -> vp.get(0).getValue().equals("Тестовый тип участка 20"))) {
+                Assertions.fail("Должна быть ошибка с типом участка 20.");
+            }
+            return;
+        }
+        Assertions.fail("Должна быть ошибка С_УУ_73.");
+    }
+
+    @Test
+    public void test_C_YY_73_2() {
+        Mockito.doReturn(new ArrayList<>()).when(moAvailableAreaTypesRepository).findAreaTypes(2L);
+        Mockito.doReturn(new ArrayList<>()).when(muAvailableAreaTypesRepository).findAreaTypes(3L);
+
+        Mockito.doReturn(Optional.of(areaType10)).when(areaTypesCRUDRepository).findById(10L);
+//        Mockito.doReturn(Optional.of(areaType20)).when(areaTypesCRUDRepository).findById(20L);
+
+        try {
+            areaServiceInternal.addMuAvailableAreaTypes(2L, 3L, Collections.singletonList(10L));
+        } catch (ContingentException e) {
+            List<ValidationMessage> validationMessageList = e.getValidation().getMessages();
+            if (validationMessageList.size() != 1) {
+                Assertions.fail("Должно отсутствовать 1 тип участков.");
+            }
+            if (!validationMessageList.stream().allMatch(vm -> vm.getCode().equals("E026"))) {
+                Assertions.fail("Должна быть ошибка только E026.");
+            }
+            if (validationMessageList.stream().map(ValidationMessage::getParameters).noneMatch(vp -> vp.get(0).getValue().equals("Тестовый тип участка 10"))) {
+                Assertions.fail("Должна быть ошибка с типом участка 10.");
+            }
+            return;
+        }
+        Assertions.fail("Должна быть ошибка С_УУ_73.");
+    }
+
+    /**
+     * С_УУ_74
+     */
+    @Test
+    public void test_C_YY_74_1() {
+        MoAvailableAreaTypes moAvailableAreaTypes10 = new MoAvailableAreaTypes();
+        moAvailableAreaTypes10.setAreaType(areaType10);
+        moAvailableAreaTypes10.setId(1L);
+        moAvailableAreaTypes10.setCreateDate(LocalDateTime.now());
+        moAvailableAreaTypes10.setMoId(2L);
+
+        Mockito.doReturn(Collections.singletonList(moAvailableAreaTypes10)).when(moAvailableAreaTypesRepository).findAreaTypes(2L);
+
+        MuAvailableAreaTypes muAvailableAreaTypes = new MuAvailableAreaTypes();
+        muAvailableAreaTypes.setId(1L);
+        muAvailableAreaTypes.setMuId(3L);
+        muAvailableAreaTypes.setAreaType(areaType10);
+        muAvailableAreaTypes.setCreateDate(LocalDateTime.now());
+        muAvailableAreaTypes.setMoAvailableAreaType(moAvailableAreaTypes10);
+
+        Mockito.doReturn(Collections.singletonList(muAvailableAreaTypes)).when(muAvailableAreaTypesRepository).findAreaTypes(3L);
+
+        try {
+            areaServiceInternal.addMuAvailableAreaTypes(2L, 3L, Collections.singletonList(10L));
+        } catch (ContingentException e) {
+            Validation validation = e.getValidation();
+            if (validation.getMessages().size() != 1) {
+                Assertions.fail("Должна быть 1 ошибка.");
+            }
+            if (!validation.getMessages().get(0).getCode().equals("E027")) {
+                Assertions.fail("Должна быть ошибка E027.");
+            }
+            if (validation.getMessages().stream().map(ValidationMessage::getParameters).noneMatch(vp -> vp.get(0).getValue().equals("Тестовый тип участка 10"))) {
+                Assertions.fail("Должна быть ошибка с типом участка 10.");
+            }
+            return;
+        }
+        Assertions.fail("Должна быть ошибка С_УУ_74.");
+    }
+
+    /**
+     * п. 3.
+     */
+    @Test
+    public void test_correct() {
+        MoAvailableAreaTypes moAvailableAreaTypes10 = new MoAvailableAreaTypes();
+        moAvailableAreaTypes10.setAreaType(areaType10);
+        moAvailableAreaTypes10.setId(1L);
+        moAvailableAreaTypes10.setCreateDate(LocalDateTime.now());
+        moAvailableAreaTypes10.setMoId(2L);
+
+        Mockito.doReturn(Collections.singletonList(moAvailableAreaTypes10)).when(moAvailableAreaTypesRepository).findAreaTypes(2L);
+
+        try {
+            areaServiceInternal.addMuAvailableAreaTypes(2L, 3L, Collections.singletonList(10L));
+        } catch (ContingentException e) {
+            Assertions.fail("Сценарий корректный, ошибки быть не должно.");
+        }
+    }
+}
---
+++ contingent/application/contingent-area/src/test/java/area/service/methods/MuAvailableAreaTypesTest.java
@@ -0,0 +1,61 @@
+package area.service.methods;
+
+import moscow.ptnl.contingent.area.entity.area.MoAvailableAreaTypes;
+import moscow.ptnl.contingent.area.entity.area.MuAvailableAreaTypes;
+import moscow.ptnl.contingent.area.entity.nsi.AreaType;
+import org.junit.jupiter.api.BeforeEach;
+import service.BaseTest;
+
+import java.time.LocalDateTime;
+
+public class MuAvailableAreaTypesTest extends BaseTest {
+
+    protected AreaType areaType10;
+    protected AreaType areaType20;
+    protected Long moId = 2L;
+    protected Long muId = 3L;
+    protected MoAvailableAreaTypes moAvailableAreaTypes10;
+    protected MoAvailableAreaTypes moAvailableAreaTypes20;
+    protected MuAvailableAreaTypes muAvailableAreaTypes;
+    protected MuAvailableAreaTypes muAvailableAreaTypes10;
+    protected MuAvailableAreaTypes muAvailableAreaTypes20;
+
+    @BeforeEach
+    public void init() {
+        areaType10 = new AreaType(10L, "Тестовый тип участка 10", false);
+        areaType20 = new AreaType(20L, "Тестовый тип участка 20", false);
+
+        moAvailableAreaTypes10 = new MoAvailableAreaTypes();
+        moAvailableAreaTypes10.setAreaType(areaType10);
+        moAvailableAreaTypes10.setId(1L);
+        moAvailableAreaTypes10.setCreateDate(LocalDateTime.now());
+        moAvailableAreaTypes10.setMoId(moId);
+
+        moAvailableAreaTypes20 = new MoAvailableAreaTypes();
+        moAvailableAreaTypes20.setAreaType(areaType10);
+        moAvailableAreaTypes20.setId(1L);
+        moAvailableAreaTypes20.setCreateDate(LocalDateTime.now());
+        moAvailableAreaTypes20.setMoId(moId);
+
+        muAvailableAreaTypes = new MuAvailableAreaTypes();
+        muAvailableAreaTypes.setId(1L);
+        muAvailableAreaTypes.setMuId(muId);
+        muAvailableAreaTypes.setAreaType(areaType20);
+        muAvailableAreaTypes.setCreateDate(LocalDateTime.now());
+        muAvailableAreaTypes.setMoAvailableAreaType(moAvailableAreaTypes20);
+
+        muAvailableAreaTypes20 = new MuAvailableAreaTypes();
+        muAvailableAreaTypes20.setId(2L);
+        muAvailableAreaTypes20.setMuId(muId);
+        muAvailableAreaTypes20.setAreaType(areaType20);
+        muAvailableAreaTypes20.setCreateDate(LocalDateTime.now());
+        muAvailableAreaTypes20.setMoAvailableAreaType(moAvailableAreaTypes20);
+
+        muAvailableAreaTypes10 = new MuAvailableAreaTypes();
+        muAvailableAreaTypes10.setId(3L);
+        muAvailableAreaTypes10.setMuId(muId);
+        muAvailableAreaTypes10.setAreaType(areaType10);
+        muAvailableAreaTypes10.setCreateDate(LocalDateTime.now());
+        muAvailableAreaTypes10.setMoAvailableAreaType(moAvailableAreaTypes10);
+    }
+}
