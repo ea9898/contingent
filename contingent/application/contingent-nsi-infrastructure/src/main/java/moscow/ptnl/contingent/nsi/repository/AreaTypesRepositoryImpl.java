63d6ac5cd2fffad97b68d8cc8143f2e089b493d8
ddryuchin <dd@ptnl.moscow>
2020-04-29 09:11:05 +0300

    CONTINGENT2-681 addMoAvailableAreaTypes. Ошибка Е002 при добавлении любого типа участка.

--- contingent/application/contingent-nsi-infrastructure/src/main/java/moscow/ptnl/contingent/nsi/repository/AreaTypesRepositoryImpl.java
+++ contingent/application/contingent-nsi-infrastructure/src/main/java/moscow/ptnl/contingent/nsi/repository/AreaTypesRepositoryImpl.java
@@ -2,6 +2,7 @@ package moscow.ptnl.contingent.nsi.repository;
 
 import moscow.ptnl.contingent.nsi.domain.area.AreaType;
 import moscow.ptnl.contingent.repository.BaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,8 +13,11 @@ import java.util.Optional;
 @Transactional(propagation= Propagation.MANDATORY)
 public class AreaTypesRepositoryImpl extends BaseRepository implements moscow.ptnl.contingent.nsi.domain.repository.AreaTypesRepository {
 
+    @Autowired
+    private AreaTypesCRUDRepository areaTypesCRUDRepository;
+
     @Override
     public Optional<AreaType> findById(Long areaTypeId) {
-        return Optional.empty();
+        return areaTypesCRUDRepository.findById(areaTypeId);
     }
 }
