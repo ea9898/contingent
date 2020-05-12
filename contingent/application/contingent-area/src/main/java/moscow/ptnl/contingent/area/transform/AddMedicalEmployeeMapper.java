0f4a8df783b8254fd6649f3ba92e114a660134a1
ddryuchin <dd@ptnl.moscow>
2020-05-12 14:41:03 +0300

    CONTINGENT2-694 setMedicalEmployeeOnArea. Не добавляется замещающий врач на участок.

--- contingent/application/contingent-area/src/main/java/moscow/ptnl/contingent/area/transform/AddMedicalEmployeeMapper.java
+++ contingent/application/contingent-area/src/main/java/moscow/ptnl/contingent/area/transform/AddMedicalEmployeeMapper.java
@@ -1,6 +1,7 @@
 package moscow.ptnl.contingent.area.transform;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 import ru.mos.emias.contingent2.core.AddMedicalEmployee;
@@ -10,10 +11,26 @@ public interface AddMedicalEmployeeMapper {
 
     AddMedicalEmployeeMapper MAPPER = Mappers.getMapper(AddMedicalEmployeeMapper.class);
 
-    @Mappings({})
+    @Mappings({
+            @Mapping(source = "medicalEmployeeJobInfoId", target = "medicalEmployeeJobInfoId"),
+            @Mapping(source = "snils", target = "snils"),
+            @Mapping(source = "positionCode", target = "positionCode"),
+            @Mapping(source = "subdivisionId", target = "subdivisionId"),
+            @Mapping(source = "replacement", target = "isReplacement"),
+            @Mapping(source = "startDate", target = "startDate"),
+            @Mapping(source = "endDate", target = "endDate")
+    })
     AddMedicalEmployee entityToDtoTransform(moscow.ptnl.contingent.domain.area.model.area.AddMedicalEmployee entityObject);
 
-    @Mappings({})
+    @Mappings({
+            @Mapping(source = "medicalEmployeeJobInfoId", target = "medicalEmployeeJobInfoId"),
+            @Mapping(source = "snils", target = "snils"),
+            @Mapping(source = "positionCode", target = "positionCode"),
+            @Mapping(source = "subdivisionId", target = "subdivisionId"),
+            @Mapping(source = "isReplacement", target = "replacement"),
+            @Mapping(source = "startDate", target = "startDate"),
+            @Mapping(source = "endDate", target = "endDate")
+    })
     moscow.ptnl.contingent.domain.area.model.area.AddMedicalEmployee dtoToEntityTransform(AddMedicalEmployee dtoObject);
 
 
