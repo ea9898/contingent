2e82b0d885331a4af77118eeffd4e807c9a98e3d
ddryuchin <dd@ptnl.moscow>
2020-10-07 11:11:19 +0300

    CONTINGENT2-913 Увеличить размер полей кодов БТИ

--- contingent/application/contingent-area/src/test/java/area/service/EsuInputMessageTest.java
+++ contingent/application/contingent-area/src/test/java/area/service/EsuInputMessageTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
-import java.io.File;
 import java.io.InputStream;
 
 public class EsuInputMessageTest {
@@ -14,7 +13,31 @@ public class EsuInputMessageTest {
     @Test
     public void jemsgTest() {
         try {
-            InputStream inputFile = EsuInputMessageTest.class.getClassLoader().getResourceAsStream("esu/JEMsg.xml");
+            InputStream inputFile = EsuInputMessageTest.class.getClassLoader().getResourceAsStream("esu/jeMsg/JEMsg.xml");
+            JAXBContext jaxbContext = JAXBContext.newInstance(JobExecutionInfoMsg.class);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            JobExecutionInfoMsg jobExecutionInfoMsg = (JobExecutionInfoMsg) unmarshaller.unmarshal(inputFile);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void jemsgTest1() {
+        try {
+            InputStream inputFile = EsuInputMessageTest.class.getClassLoader().getResourceAsStream("esu/jeMsg/JEMsg1.xml");
+            JAXBContext jaxbContext = JAXBContext.newInstance(JobExecutionInfoMsg.class);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            JobExecutionInfoMsg jobExecutionInfoMsg = (JobExecutionInfoMsg) unmarshaller.unmarshal(inputFile);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void jemsgTest2() {
+        try {
+            InputStream inputFile = EsuInputMessageTest.class.getClassLoader().getResourceAsStream("esu/jeMsg/JEMsg2.xml");
             JAXBContext jaxbContext = JAXBContext.newInstance(JobExecutionInfoMsg.class);
             Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
             JobExecutionInfoMsg jobExecutionInfoMsg = (JobExecutionInfoMsg) unmarshaller.unmarshal(inputFile);
