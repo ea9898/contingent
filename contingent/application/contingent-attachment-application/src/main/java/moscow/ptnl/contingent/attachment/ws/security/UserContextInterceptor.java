f6fe8abac3e043f54664ec4e9e456e96d765acbe
ddryuchin <dd@ptnl.moscow>
2020-12-08 11:24:01 +0300

    Исправление ошибки при отсутствии action в Content-Type

--- contingent/application/contingent-attachment-application/src/main/java/moscow/ptnl/contingent/attachment/ws/security/UserContextInterceptor.java
+++ contingent/application/contingent-attachment-application/src/main/java/moscow/ptnl/contingent/attachment/ws/security/UserContextInterceptor.java
@@ -5,6 +5,8 @@ import org.apache.cxf.binding.soap.interceptor.AbstractSoapInterceptor;
 import org.apache.cxf.headers.Header;
 import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.phase.Phase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.w3c.dom.Element;
 import ru.mos.emias.system.v1.usercontext.UserContext;
 
@@ -14,6 +16,8 @@ import javax.xml.bind.Unmarshaller;
 import javax.xml.namespace.QName;
 
 public class UserContextInterceptor extends AbstractSoapInterceptor {
+
+    private final static Logger LOG = LoggerFactory.getLogger(UserContextInterceptor.class);
 
     public UserContextInterceptor() {
         super(Phase.PRE_PROTOCOL);
@@ -42,14 +46,21 @@ public class UserContextInterceptor extends AbstractSoapInterceptor {
 
         // На всякий случай, контекст может быть не задан, а в БД поле имени пользователя обязательное
         if (userContext.getUserName() == null) { userContext.setUserName("Не задано");}
-        
-        String methodName = soapMessage
-                .getExchange()
-                .getBindingOperationInfo()
-                .getOperationInfo()
-                .getName()
-                .getLocalPart();
-        
+
+        String methodName = null;
+        try {
+            methodName = soapMessage
+                    .getExchange()
+                    .getBindingOperationInfo()
+                    .getOperationInfo()
+                    .getName()
+                    .getLocalPart();
+        } catch (Exception e) {
+            LOG.error("Ошибка получения имени вызываемого метода", e);
+            throw new RuntimeException("Не определен action");
+        }
+
         UserContextHolder.setContext(new RequestContext(methodName, userContext));
+
     }
 }
