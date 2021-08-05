a57cda72d94238b78a91fc1b04c0adae4ed99c48
Данил Дрючин <danil.dryuchin@ptnl.moscow>
2021-08-05 10:53:09 +0300

    Исправление маппинга приказов

--- contingent/application/contingent-area/src/main/java/moscow/ptnl/contingent/area/transform/v2/AddressAllocationOrder2MapperV2.java
+++ contingent/application/contingent-area/src/main/java/moscow/ptnl/contingent/area/transform/v2/AddressAllocationOrder2MapperV2.java
@@ -3,6 +3,7 @@ package moscow.ptnl.contingent.area.transform.v2;
 import moscow.ptnl.contingent.domain.area.entity.AddressAllocationOrders;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
 import ru.mos.emias.contingent2.core.v2.Order;
@@ -11,6 +12,7 @@ import ru.mos.emias.contingent2.core.v2.Order;
 public abstract class AddressAllocationOrder2MapperV2 {
 
     @Mappings({
+            @Mapping(target = "createDate", expression = "java( entity.getCreateDate().toLocalDate() )")
     })
     public abstract Order entityToDtoTransform(AddressAllocationOrders entity);
 }
