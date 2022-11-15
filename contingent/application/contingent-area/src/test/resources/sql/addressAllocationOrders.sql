c40b6cbf5188226b60600718e43989b9ad2e10a6
Evgeniy Aleksandrovich <ea9898@yandex.ru>
2022-11-15 10:35:53 +0300

    CONTINGENT2-2458 Доработка метода addMoAddress (работа с новым алгоритмом поиска адресов) + sql скрипты

---
+++ contingent/application/contingent-area/src/test/resources/sql/addressAllocationOrders.sql
@@ -0,0 +1,25 @@
+INSERT INTO address_allocation_orders (
+            id,
+            number,
+            name,
+            date,
+            ouz,
+            create_date,
+            update_date,
+            archived)
+VALUES (1, '93691', 'Name.Order.Update', TO_TIMESTAMP('2021-06-25 16:14:46.965000', 'YYYY-MM-DD HH24:MI:SS.FF6'),
+'OUZ.Update', TO_TIMESTAMP('2021-06-25 16:14:46.965000', 'YYYY-MM-DD HH24:MI:SS.FF6'),
+TO_TIMESTAMP('2021-06-25 16:14:46.965000', 'YYYY-MM-DD HH24:MI:SS.FF6'), 0);
+
+INSERT INTO address_allocation_orders (
+            id,
+            number,
+            name,
+            date,
+            ouz,
+            create_date,
+            update_date,
+            archived)
+VALUES (2, '93691', 'Name.Order.Update', TO_TIMESTAMP('2021-06-25 16:14:46.965000', 'YYYY-MM-DD HH24:MI:SS.FF6'),
+'OUZ.Update', TO_TIMESTAMP('2021-06-25 16:14:46.965000', 'YYYY-MM-DD HH24:MI:SS.FF6'),
+TO_TIMESTAMP('2021-06-25 16:14:46.965000', 'YYYY-MM-DD HH24:MI:SS.FF6'), 0);
